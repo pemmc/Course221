3181a3ee5295d1ad432be3d376b55b34ea3262ee
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-27 11:54:55 -0300

    Usando o delegate, programa antes de usar

---
+++ Course221Delegate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Course221Delegate.Services;
+
+namespace Course221Delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10;
+            double y = 12;
+
+            double result1 = CalculationService.Max(x, y);
+
+            double result2 = CalculationService.Sum(x, y);
+
+            double result3 = CalculationService.Square(x);
+
+            Console.WriteLine(result1);
+
+            Console.WriteLine(result2);
+
+            Console.WriteLine(result3);
+
+        }
+    }
+}
+
+
